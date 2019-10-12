92c9da0992ef931ecc95cfe4d2803f17d0295636
viperx27 <13475518@student.uts.edu.au>
2019-10-12 20:02:16 +1100

    Second commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Menu : MonoBehaviour
+{
+    Button button;
+    // private GameObject g;
+    private GameObject h;
+
+
+
+
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("Tetris");
+        DontDestroyOnLoad(gameObject);
+       
+
+        Debug.Log("Listner Added");
+    }
+
+
+  
+
+    
+
+
+
+
+
+
+    void Start()
+    {
+       
+
+
+
+    }
+
+    void Update()
+    {
+
+        
+    }
+
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+
+}
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+
+    bool Pause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+
+
+
+
+
+    void Update()
+    {
+
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+
+                Pause = true;
+
+            }
+
+            else if (Input.GetMouseButton(1))
+            {
+                if(Time.timeScale == 0)
+                {
+                    Time.timeScale = 1;
+                    Pause = false;
+                }
+            }
+        }
+
+
+     
+  
+       }
+    }
+   
+    
+
+
+
+
